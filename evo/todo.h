5d21df8971511a7e6b18432a8a1c2bd6180e34b1
insage1337 <68876628+insage1337@users.noreply.github.com>
2024-01-30 03:46:17 +0300

    Create todo.h

---
+++ evo/todo.h
@@ -0,0 +1,47 @@
+/* todo fix entity dissapears while knife change aimbotz (fullupdate??)
+* weapon config - settings //done
+* curve aimbot
+* virtual memory crash
+* nearest hitbox
+* fix memory leak //done
+* rename functions
+* crypt strings
+* kill delay
+* add triggeraim
+* add fullscreen support
+* fix draw fov 
+* add deathmatch for aimbot
+* add submachine gun settings //done
+* fix rcs fov //done
+* fix awp & pistol rcs //done
+* hide thread //done
+* fix rcs always enabled
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
