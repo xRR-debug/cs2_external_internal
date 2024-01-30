c88ee0c6e9c750dfdcf63e0e5dbc3e4f8f3b3010
insage1337 <68876628+insage1337@users.noreply.github.com>
2024-01-30 03:51:25 +0300

    Add files via upload

---
+++ evo/utils/os-imgui/custom.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#define  IMGUI_DEFINE_MATH_OPERATORS
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+
+#include <string>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+#define to_vec4( r, g, b, a ) ImColor( r / 255.f, g / 255.f, b / 255.f, a )
+
+class c_custom {
+
+public:
+    float m_anim = 0.f;
+
+    int m_tab = 0;
+    vector < const char* > tabs = { "Legitbot" }, tabs_icons = { "B" };
+
+    int m_legitbot_subtab = 0;
+    vector < const char* > legitbot_subtabs = { "Aimbot", "Triggerbot", "Visuals", "Misc", "Config" };
+
+    float col_buf[4] = { 1.f, 1.f, 1.f, 1.f };
+
+    void render_arrows_for_horizontal_bar(ImVec2 pos, float alpha, float width, float height);
+
+    void prepared_popup(const char* id, const char* name, std::function<void()> content);
+    void group_box_alternative(const char* name, ImVec2 size_arg, ImVec2 padding = ImVec2(12, 12));
+    void end_group_box_alternative();
+    bool settings_widget(const char* u_id);
+
+    bool expand_button(const char* label, bool selected, float rounding = 0.f, ImDrawFlags flags = NULL);
+    bool tab(const char* icon, const char* label, bool selected);
+    bool sub_tab(const char* label, bool selected);
+
+    void tab_area(const char* str_id, ImVec2 size, std::function < void() > content);
+
+    void begin_child(const char* name, ImVec2 size);
+    void end_child();
+
+    int accent_color[4] = { 76, 222, 252, 255 };
+    ImColor get_accent_color(float a = 1.f) {
+
+        return to_vec4(accent_color[0], accent_color[1], accent_color[2], a);
+    }
+
+};
+
+inline c_custom custom;
