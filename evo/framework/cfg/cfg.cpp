b387c1a9d9feb3f0f96041c4902313a1e90c090f
insage1337 <68876628+insage1337@users.noreply.github.com>
2024-02-17 17:39:31 +0300

    changed variables

--- evo/framework/cfg/cfg.cpp
+++ evo/framework/cfg/cfg.cpp
@@ -1,64 +1,125 @@
-#include "cfg.hpp"
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+#include <shlobj_core.h>
 
-bool angel::settings_t::save(std::string file_name) 
+#include "../../sdk/classes/bone.h"
+#include "../../core/ctx/ctx.h"
+#include "../../utils/random.h"
+
+namespace path
 {
-	std::string file_path = path::path + file_name + "";
-
-	std::fstream file(file_path, std::ios::out | std::ios::in | std::ios::trunc);
-	file.close();
-
-	file.open(file_path, std::ios::out | std::ios::in);
-	if (!file.is_open()) {
-		file.close();
-		return false;
-	}
-
-	const size_t settings_size = sizeof(angel::settings_t);
-	for (int i = 0; i < settings_size; i++) {
-		byte current_byte = *reinterpret_cast<byte*>(uintptr_t(this) + i);
-		for (int x = 0; x < 8; x++) {
-			file << (int)((current_byte >> x) & 1);
-		}
-	}
-
-	file.close();
-
-	return true;
+	inline std::string path = "";
 }
 
-bool angel::settings_t::load(std::string file_name) 
-{
-	std::string file_path = path::path + file_name + "";
+namespace angel {
+	class settings_t {
+	public:
+		bool save(std::string file_name);
+		bool load(std::string file_name);
+		//main
 
-	std::fstream file;
-	file.open(file_path, std::ios::out | std::ios::in);
-	if (!file.is_open()) {
-		file.close();
-		return false;
-	}
+		bool aimbot = false;
+		bool aimbot_always = false;
+		bool aimbot_toggle_mode = false;
+		int aimbot_hotkey = 0;
+		int  aim_position = 0;
+		DWORD  aim_hitbox_position = BONEINDEX::head;
+		bool aimbot_visible_check = true;
+		bool aimbot_curve = false;
+		bool aimbot_scopecheck = true;
 
-	std::string line;
-	while (file) {
-		std::getline(file, line);
+		bool rcs = false;
 
-		const size_t settings_size = sizeof(angel::settings_t);
-		if (line.size() > settings_size * 8) {
-			file.close();
-			return false;
-		}
-		for (int i = 0; i < settings_size; i++) {
-			byte current_byte = *reinterpret_cast<byte*>(uintptr_t(this) + i);
-			for (int x = 0; x < 8; x++) {
-				if (line[(i * 8) + x] == '1')
-					current_byte |= 1 << x;
-				else
-					current_byte &= ~(1 << x);
-			}
-			*reinterpret_cast<byte*>(uintptr_t(this) + i) = current_byte;
-		}
-	}
+		bool weapon_config = false;
 
-	file.close();
+		float rifle_fov = 0;
+		float rifle_smooth = 0;
 
-	return true;
+		float pistol_fov = 0;
+		float pistol_smooth = 0;
+
+		float sniper_fov = 0;
+		float sniper_smooth = 0;
+
+		float heavysmg_fov = 0;
+		float heavysmg_smooth = 0;
+
+		int box_type = 0;
+		int  healthbar_type = 0;
+		float aimbotfov = 0;
+		float rcsfov = 0;
+
+		ImColor fov_circle_color = ImColor(255, 255, 255, 255);
+
+		bool show_menu = true;
+
+		bool triggerbot = false;
+		bool triggerbot_always = false;
+		int triggerbot_hotkey = 0;
+
+		bool team_check = true;
+
+		bool crosshair_placement = false;
+		ImColor crosshair_placement_color = ImColor(255, 255, 255, 200);
+
+		bool obsbypass = true;
+		//esp
+		 bool esp_enabled = true;
+		 bool ammobar = false;
+		 bool bone_esp = false;
+		 bool box_esp = true;
+		 bool healthbar = true;
+		 bool weapon_esp = true;
+		 bool scoped_esp = true;
+		 bool player_name = true;
+		 bool draw_fov = false;
+		 bool show_distance = false;
+		 bool esp_visible_check = true;
+		 bool filled_box = false;
+		 bool filled_vis_box = false;
+		 bool multi_color = true;
+		 bool outLine = false;
+		 float box_rounding = 3.0;
+		 float box_alpha = 0.35;
+
+		 ImColor bone_color = ImColor(0, 0, 0, 255);
+		 ImColor box_color = ImColor(0, 255, 0, 255);
+		 ImColor visible_color = ImColor(0, 0, 0, 255);
+		 ImColor filled_color = ImColor(255, 255, 255, 128);
+		 ImColor filled_vis_color = ImColor(0, 167, 255, 255);
+		 ImColor filled_color2 = ImColor(167, 50, 255, 255);
+		//crosshair
+		 bool crosshair_team_check = true;
+		 bool is_aim = false;
+		 bool is_jump = false;
+		//radar
+		bool show_radar = false;
+		float radar_range = 150;
+		float radar_point_size_proportion = 1;
+		bool show_radar_crossline = false;
+		ImColor radar_crossline_color = ImColor(255, 255, 255, 255);
+		int radar_type = 2;
+		float proportion = 3300;
+		bool custom_radar = false;
+		float radar_bg_alpha = 0.1 ;
+		//misc
+		bool bunnyhop = false;
+		bool work_in_spec = true;
+		bool watermark = false;
+		bool cheat_list = false;
+		bool hitsound = false;
+		int hitsound_type = 0;
+		bool bomb_timer = false;
+		bool discordrpc = true;
+		bool unload = false;
+		ImColor bomb_timer_color = ImColor(255, 120, 0, 255);
+	};
+
+	inline const auto _settings = std::make_unique< settings_t >();
 }
